aeee73a2175fb2d087087f43bbeae0e6180a1a3e
yzqiang666 <yzqiang6@gmail.com>
2021-11-06 21:15:56 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
   ENTRYPOINT_URL="https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
-  wget -O  /tmp/entrypoint1.sh https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh
-  [ ! $? == 0 ] &&   wget -O  /tmp/entrypoint1.sh https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh
-  [ ! $? == 0 ] &&   wget -O  /tmp/entrypoint1.sh https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh
-  [ ! $? == 0 ] &&   wget -O  /tmp/entrypoint1.sh https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh
+  curl -kL -o  /tmp/entrypoint1.sh "https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
+  [ ! $? == 0 ] &&   curl -kL -o  /tmp/entrypoint1.sh "https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
+  [ ! $? == 0 ] &&   curl -kL -o  /tmp/entrypoint1.sh "https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
+  [ ! $? == 0 ] &&   curl -kL -o  /tmp/entrypoint1.sh "https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
   if [ -s /tmp/entrypoint1.sh ]  ; then
     echo "Download from url ${ENTRYPOINT_URL} file success." 
     chmod +x /tmp/entrypoint1.sh
