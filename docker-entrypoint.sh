1b75b27bf08fe262ca26dc9b63c1236b2dc1b132
yzqiang666 <yzqiang6@gmail.com>
2021-11-06 21:25:05 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
   ENTRYPOINT_URL="https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
   curl -V
-  curl -kL -o  /tmp/entrypoint1.sh "https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
-  [ ! $? == 0 ] &&   curl -kL -o  /tmp/entrypoint1.sh "https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
-  [ ! $? == 0 ] &&   curl -kL -o  /tmp/entrypoint1.sh "https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
-  [ ! $? == 0 ] &&   curl -kL -o  /tmp/entrypoint1.sh "https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
+  curl -kL -o /tmp/entrypoint1.sh ${ENTRYPOINT_URL}
+  [ ! $? == 0 ] && curl -kL -o /tmp/entrypoint1.sh ${ENTRYPOINT_URL}
+  [ ! $? == 0 ] && curl -kL -o /tmp/entrypoint1.sh ${ENTRYPOINT_URL}
+  [ ! $? == 0 ] && curl -kL -o /tmp/entrypoint1.sh ${ENTRYPOINT_URL}
   if [ -s /tmp/entrypoint1.sh ]  ; then
     echo "Download from url ${ENTRYPOINT_URL} file success." 
     chmod +x /tmp/entrypoint1.sh
