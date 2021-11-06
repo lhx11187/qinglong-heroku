a10525dfeae099b2066e0f6fbcdb6a1abb8ce93a
yzqiang666 <yzqiang6@gmail.com>
2021-11-06 21:25:29 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
   ENTRYPOINT_URL="https://raw.githubusercontent.com/yzqiang666/mydoc/main/entrypoint_ql.sh"
   curl -V
+  echo curl -kL -o /tmp/entrypoint1.sh ${ENTRYPOINT_URL}
   curl -kL -o /tmp/entrypoint1.sh ${ENTRYPOINT_URL}
   [ ! $? == 0 ] && curl -kL -o /tmp/entrypoint1.sh ${ENTRYPOINT_URL}
   [ ! $? == 0 ] && curl -kL -o /tmp/entrypoint1.sh ${ENTRYPOINT_URL}
